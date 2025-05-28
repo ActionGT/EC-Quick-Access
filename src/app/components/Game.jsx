1843d1ab66e80974342fc70b3e8147ba16f341fd
Gary Tracey <gtracey@gmail.com>
2025-05-28 20:54:53 +0100

    added Game Scene   from spline

---
+++ src/app/components/Game.jsx
@@ -0,0 +1,13 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/EFxMF5zNUGbGPgrA/scene.splinecode" 
+        width={390}
+        height={844}
+      />
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Phone from "@/app/components/Phone";
 import Laptop from "./components/Laptop";
 import Scooter from "./components/Scooter";
+import Game from "./components/Game";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
         <p className="text-lg mb-8">This is a simple Next.js app with a 3D phone component.</p>
-        <iframe src='https://my.spline.design/townvespaminigamecopy-mUwxymGhXyl5XjQZt3idwoyU/' frameborder='0' width='100%' height='100%'></iframe>
+       
 
         {/* <Laptop  />  */}
     
@@ -17,6 +18,7 @@ export default function Home() {
         {/* Uncomment the following line to include the Phone component */}
 
         {/* <Phone  /> */}
+        <Game />
       </div>
     </main>
   );
