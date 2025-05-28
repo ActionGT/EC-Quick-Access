8ebbe16bce68d7a9b4a037683e455d973b5286f1
Gary Tracey <gtracey@gmail.com>
2025-05-28 20:46:22 +0100

    added scooter embed  from spline

--- src/app/components/Scooter.jsx
+++ src/app/components/Scooter.jsx
@@ -3,9 +3,11 @@ import Spline from '@splinetool/react-spline/next';
 export default function Scooter() {
   return (
     <main>
-      <Spline
+      {/* <Spline
         scene="https://prod.spline.design/ruGXpw650YqMcG1q/scene.splinecode" 
-      />
+      /> */}
+      <iframe src='https://my.spline.design/townvespaminigamecopy-mUwxymGhXyl5XjQZt3idwoyU/' frameborder='0' width='100%' height='100%'></iframe>
+      
     </main>
   );
 }
