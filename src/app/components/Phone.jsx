604dbcf84ed1b87a6a360c59756e802f13f80159
actiongt <gtracey@gmail.com>
2025-05-28 17:59:20 +0100

    First commitr

---
+++ src/app/components/Phone.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Phone() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/z7-IhUR0hnzqZeoK/scene.splinecode" 
+      />
+    </main>
+  );
+}
