089a89d4459de6f42e87900b1ede9c006d8a3a61
Gary Tracey <gtracey@gmail.com>
2025-05-28 21:01:40 +0100

    Removed divs from page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,20 +6,22 @@ import Game from "./components/Game";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
-        <p className="text-lg mb-8">This is a simple Next.js app with a 3D phone component.</p>
+
+     <Game />
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   <div className="flex flex-col items-center justify-center">
+    //     <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
+    //     <p className="text-lg mb-8">This is a simple Next.js app with a 3D phone component.</p>
        
 
-        {/* <Laptop  />  */}
+    //     {/* <Laptop  />  */}
     
-        {/* <Scooter /> */}
-        {/* Uncomment the following line to include the Phone component */}
+    //     {/* <Scooter /> */}
+    //     {/* Uncomment the following line to include the Phone component */}
 
-        {/* <Phone  /> */}
-        <Game />
-      </div>
-    </main>
+    //     {/* <Phone  /> */}
+       
+    //   </div>
+    // </main>
   );
 }
